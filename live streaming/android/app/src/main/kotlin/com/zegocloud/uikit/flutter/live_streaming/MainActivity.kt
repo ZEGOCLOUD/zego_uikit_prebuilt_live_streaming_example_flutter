8d5c763c2ca6a76356a668ef4a22d14993e37ff5
yuyanjun <yuyanjun@zego.im>
2022-09-09 19:02:44 +0800

    update app name

--- live streaming/android/app/src/main/kotlin/com/zegocloud/uikit/flutter/live_streaming/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.zegocloud.uikit.flutter.live_streaming
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- live_streaming/lib/main.dart
+++ live_streaming/lib/main.dart
@@ -113,7 +113,6 @@ class LivePage extends StatelessWidget {
                   ]
                 : const [],
           ),
-          useEndLiveStreamingButton: isHost,
         ),
       ),
     );
