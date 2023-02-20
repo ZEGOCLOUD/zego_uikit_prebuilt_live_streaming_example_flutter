1ee68f59ba02bc05fa5a3331c113aa7b017a95b4
nemoyuan <nemoyuan@zego.im>
2023-02-20 15:39:58 +0800

    Support PK Battles.

---
+++ live_streaming_with_pkbattles/android/app/src/main/kotlin/com/zegocloud/live_streaming_with_cohosting_and_pk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zegocloud.live_streaming_with_pkbattles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ live_streaming_with_pkbattles/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,63 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_logs/FlutterLogsPlugin.h>)
+#import <flutter_logs/FlutterLogsPlugin.h>
+#else
+@import flutter_logs;
+#endif
+
+#if __has_include(<native_device_orientation/NativeDeviceOrientationPlugin.h>)
+#import <native_device_orientation/NativeDeviceOrientationPlugin.h>
+#else
+@import native_device_orientation;
+#endif
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
+#endif
+
+#if __has_include(<wakelock/WakelockPlugin.h>)
+#import <wakelock/WakelockPlugin.h>
+#else
+@import wakelock;
+#endif
+
+#if __has_include(<zego_express_engine/ZegoExpressEnginePlugin.h>)
+#import <zego_express_engine/ZegoExpressEnginePlugin.h>
+#else
+@import zego_express_engine;
+#endif
+
+#if __has_include(<zego_zim/ZegoZimPlugin.h>)
+#import <zego_zim/ZegoZimPlugin.h>
+#else
+@import zego_zim;
+#endif
+
+#if __has_include(<zego_zpns/ZegoZpnsPlugin.h>)
+#import <zego_zpns/ZegoZpnsPlugin.h>
+#else
+@import zego_zpns;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterLogsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterLogsPlugin"]];
+  [NativeDeviceOrientationPlugin registerWithRegistrar:[registry registrarForPlugin:@"NativeDeviceOrientationPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [WakelockPlugin registerWithRegistrar:[registry registrarForPlugin:@"WakelockPlugin"]];
+  [ZegoExpressEnginePlugin registerWithRegistrar:[registry registrarForPlugin:@"ZegoExpressEnginePlugin"]];
+  [ZegoZimPlugin registerWithRegistrar:[registry registrarForPlugin:@"ZegoZimPlugin"]];
+  [ZegoZpnsPlugin registerWithRegistrar:[registry registrarForPlugin:@"ZegoZpnsPlugin"]];
+}
+
+@end
