efc8d8ced1a3f08edd1675fcd2748dab36f3284a
nemo <jzyeran@gmail.com>
2023-12-11 18:59:22 +0800

    update demo

--- live_streaming_with_minigame/lib/constants.dart
+++ live_streaming_with_minigame/lib/constants.dart
@@ -1,9 +1,9 @@
 // Dart imports:
 import 'dart:math' as math;
 
-const int yourAppID =1 ;
-const String yourAppSign = '';
-const String yourServerSecret = '';
+const int yourAppID = ;
+const String yourAppSign = ;
+const String yourServerSecret = ;
 
 /// Note that the userID needs to be globally unique,
 final String localUserID = math.Random().nextInt(1000000).toString();
